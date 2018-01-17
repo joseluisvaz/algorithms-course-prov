101e3a877e1eb4d2281657ffceafe1f04b2bfbc4
joseluisvaz <joseluis.vaz.e@gmail.com>
2018-01-16 19:05:15 -0600

    Implement `fibonacci_fast(int n)`|

--- assignment1/fibonacci/fibonacci.cpp
+++ assignment1/fibonacci/fibonacci.cpp
@@ -15,31 +15,41 @@
 //    and submit it to the grader.
 
 int fibonacci_naive(int n) {
-    if (n <= 1)
-        return n;
+  if (n <= 1)
+    return n;
 
-    return fibonacci_naive(n - 1) + fibonacci_naive(n - 2);
+  return fibonacci_naive(n - 1) + fibonacci_naive(n - 2);
 }
 
-int fibonacci_fast(int n) {
-    // write your code here
+long long fibonacci_fast(int n) {
+  if (n <= 1)
+    return n;
 
-    return 0;
+  long long previous = 0;
+  long long current = 1;
+
+  for (int i = 2; i < n + 1; ++i) {
+    long long next = current + previous;
+    previous = current;
+    current = next;
+  }
+
+  return current;
 }
 
 void test_solution() {
-    assert(fibonacci_fast(3) == 2);
-    assert(fibonacci_fast(10) == 55);
-    for (int n = 0; n < 20; ++n)
-        assert(fibonacci_fast(n) == fibonacci_naive(n));
+  assert(fibonacci_fast(3) == 2);
+  assert(fibonacci_fast(10) == 55);
+  for (int n = 0; n < 20; ++n)
+    assert(fibonacci_fast(n) == fibonacci_naive(n));
 }
 
 int main() {
-    int n = 0;
-    std::cin >> n;
+  int n = 0;
+  std::cin >> n;
 
-    std::cout << fibonacci_naive(n) << '\n';
-    //test_solution();
-    //std::cout << fibonacci_fast(n) << '\n';
-    return 0;
+  //std::cout << fibonacci_naive(n) << '\n';
+  //test_solution();
+  std::cout << fibonacci_fast(n) << '\n';
+  return 0;
 }
