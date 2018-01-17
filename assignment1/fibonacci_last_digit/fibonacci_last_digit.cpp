823bad3c9c0b491ce7ff7eb196cd0ba40d1aaf27
joseluisvaz <joseluis.vaz.e@gmail.com>
2018-01-16 19:43:44 -0600

    Implement `fibonacci_last_digit_fast(int n)`

--- assignment1/fibonacci_last_digit/fibonacci_last_digit.cpp
+++ assignment1/fibonacci_last_digit/fibonacci_last_digit.cpp
@@ -1,24 +1,40 @@
 #include <iostream>
 
 int get_fibonacci_last_digit_naive(int n) {
-    if (n <= 1)
-        return n;
+  if (n <= 1)
+    return n;
 
-    int previous = 0;
-    int current  = 1;
+  int previous = 0;
+  int current = 1;
 
-    for (int i = 0; i < n - 1; ++i) {
-        int tmp_previous = previous;
-        previous = current;
-        current = tmp_previous + current;
-    }
+  for (int i = 0; i < n - 1; ++i) {
+    int tmp_previous = previous;
+    previous = current;
+    current = tmp_previous + current;
+  }
 
-    return current % 10;
+  return current % 10;
+}
+
+int get_fibonacci_last_digit_fast(int n) {
+  if (n < 1)
+    return (n);
+
+  int previous = 0;
+  int current = 1;
+
+  for (int i = 2; i < n + 1; ++i) {
+    int next = (previous + current) % 10;
+    previous = current;
+    current = next;
+  }
+
+  return current;
 }
 
 int main() {
-    int n;
-    std::cin >> n;
-    int c = get_fibonacci_last_digit_naive(n);
-    std::cout << c << '\n';
-    }
+  int n;
+  std::cin >> n;
+  int c = get_fibonacci_last_digit_fast(n);
+  std::cout << c << '\n';
+}
